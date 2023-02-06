4af1c066dc8eec476d0fc5faf8cc7ec27fceac2e
Slava <110696118+sl7one@users.noreply.github.com>
2023-02-06 17:38:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { Container } from 'components/components.styled';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Container>
+  <React.StrictMode>
     <App />
-  </Container>
-
-  // </React.StrictMode>
+  </React.StrictMode>
 );
